8332b13a91ed2d82417bc1a8714f16c3de7c53e0
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2024-03-27 22:32:46 -0600

    💫 feat(component) Header create

--- app/tailwind.config.mjs
+++ app/tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(192, 70%, 51%)",
+        secondary: "hsl(136, 65%, 51%)",
+      },
+    },
+  },
+  plugins: [],
+};
